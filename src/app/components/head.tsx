1f0c8cb423a5350d759c3bed26642f32f4c60703
pruthviraj-sawant <sawantpruthviraj1@gmail.com>
2025-03-16 10:06:58 +0530

    finalll

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -21,7 +21,7 @@ export default function ThreeDCardDemo() {
       <div className="flex flex-wrap justify-center gap-2 mt-14">
         {/* Card 1 */}
         <CardContainer className="inter-var ml-12 mr-5 ">
-          <CardBody  className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.9] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[18rem] h-[30rem] rounded-xl p-6 border">
+          <CardBody  className=" relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.9] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[18rem] h-[30rem] rounded-xl p-6 border">
             <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
               HEAD
             </CardItem>
